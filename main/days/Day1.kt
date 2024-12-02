9df2f8a7875e56142e3fe6a25233a2c436890fa6
Hannes Güdelhöfer <hannes@guedelhoefer.de>
2024-12-02 09:16:33 +0100

    Day  2

--- main/days/Day1.kt
+++ main/days/Day1.kt
@@ -4,40 +4,35 @@ import me.reckter.aoc.Day
 import me.reckter.aoc.parseWithRegex
 import me.reckter.aoc.solution
 import me.reckter.aoc.solve
-import me.reckter.aoc.toIntegers
 import kotlin.math.abs
-import kotlin.math.max
-import kotlin.math.min
 
 class Day1 : Day {
-	override val day = 1
+    override val day = 1
 
-	val lists by lazy {
-		loadInput()
-			.parseWithRegex("(\\d+) *(\\d+)")
-			.map { (firstStr, secondStr) -> firstStr.toInt() to secondStr.toInt() }
-			.unzip()
-			.toList()
-			.map { it.sorted() }
-	}
+    val lists by lazy {
+        loadInput()
+            .parseWithRegex("(\\d+) *(\\d+)")
+            .map { (firstStr, secondStr) -> firstStr.toInt() to secondStr.toInt() }
+            .unzip()
+            .toList()
+            .map { it.sorted() }
+    }
 
-	override fun solvePart1() {
-		lists
-			.let {
-				it.first().zip(it.last())
-			}
-			.sumOf { (first, second) -> abs(first - second) }
-			.solution(1)
+    override fun solvePart1() {
+        lists
+            .let {
+                it.first().zip(it.last())
+            }.sumOf { (first, second) -> abs(first - second) }
+            .solution(1)
+    }
 
-	}
-
-	override fun solvePart2() {
-		val (first, second) = lists
-		first.sumOf { num ->
-			num * second.count { num == it }
-		}.solution(2)
-
-	}
+    override fun solvePart2() {
+        val (first, second) = lists
+        first
+            .sumOf { num ->
+                num * second.count { num == it }
+            }.solution(2)
+    }
 }
 
 fun main() = solve<Day1>()
