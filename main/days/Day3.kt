0792ee5dbb366447cd137e791938ff6a2b7a6fc5
Hannes Güdelhöfer <hannes@guedelhoefer.de>
2024-12-03 09:43:58 +0100

    Add Day 3

---
+++ main/days/Day3.kt
@@ -0,0 +1,40 @@
+package me.reckter.aoc.days
+
+import me.reckter.aoc.Day
+import me.reckter.aoc.solution
+import me.reckter.aoc.solve
+
+class Day3 : Day {
+    override val day = 3
+
+    val regex = Regex("mul\\((\\d+),(\\d+)\\)")
+
+    override fun solvePart1() {
+        val str =
+            loadInput(trim = false)
+                .joinToString("\n")
+
+        regex
+            .findAll(str)
+            .map { match -> match.groupValues[1].toInt() * match.groupValues[2].toInt() }
+            .sum()
+            .solution(1)
+    }
+
+    override fun solvePart2() {
+        val str =
+            loadInput(trim = false)
+                .joinToString("\n")
+                .split("do()")
+                .map { it.substringBefore("don't()") }
+                .joinToString(" ")
+
+        regex
+            .findAll(str)
+            .map { match -> match.groupValues[1].toInt() * match.groupValues[2].toInt() }
+            .sum()
+            .solution(1)
+    }
+}
+
+fun main() = solve<Day3>()
