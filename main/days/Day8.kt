387f1d52f81d332f76322e909ad3df07b4297967
Hannes Güdelhöfer <hannes@guedelhoefer.de>
2024-12-08 19:48:14 +0100

    Add day 8

---
+++ main/days/Day8.kt
@@ -0,0 +1,68 @@
+package me.reckter.aoc.days
+
+import me.reckter.aoc.Day
+import me.reckter.aoc.allPairings
+import me.reckter.aoc.cords.d2.Cord2D
+import me.reckter.aoc.cords.d2.minus
+import me.reckter.aoc.cords.d2.plus
+import me.reckter.aoc.gcd
+import me.reckter.aoc.parseMap
+import me.reckter.aoc.solution
+import me.reckter.aoc.solve
+
+class Day8 : Day {
+    override val day = 8
+
+    val map by lazy {
+        loadInput()
+            .parseMap { it }
+    }
+
+    override fun solvePart1() {
+        map.entries
+            .filter { it.value != '.' }
+            .groupBy { it.value }
+            .mapValues { it.value.map { it.key } }
+            .entries
+            .flatMap {
+                it.value
+                    .allPairings(bothDirections = true)
+                    .map { (first, second) ->
+                        val distance = first - second
+                        first + distance
+                    }
+            }.distinct()
+            .filter { map.contains(it) }
+            .count()
+            .solution(1)
+    }
+
+    override fun solvePart2() {
+        map.entries
+            .filter { it.value != '.' }
+            .groupBy { it.value }
+            .mapValues { it.value.map { it.key } }
+            .entries
+            .flatMap {
+                it.value
+                    .allPairings()
+                    .flatMap { (first, second) ->
+                        val distance = first - second
+                        val factor = gcd(distance.x.toLong(), distance.y.toLong()).toInt()
+                        val direction = Cord2D(distance.x / factor, distance.y / factor)
+                        val oneDirection =
+                            generateSequence(first) { it + direction }
+                                .takeWhile { map.contains(it) }
+                        val otherDirection =
+                            generateSequence(first - direction) { it - direction }
+                                .takeWhile { map.contains(it) }
+                        oneDirection + otherDirection
+                    }
+            }.distinct()
+            .filter { map.contains(it) }
+            .count()
+            .solution(2)
+    }
+}
+
+fun main() = solve<Day8>()
