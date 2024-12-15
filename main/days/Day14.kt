b8aebff5c3a5f52effe9384f946b1b45bdc7bf4b
Hannes Güdelhöfer <hannes@guedelhoefer.de>
2024-12-15 21:54:03 +0100

    Add Day 15

--- main/days/Day14.kt
+++ main/days/Day14.kt
@@ -11,143 +11,147 @@ import me.reckter.aoc.solve
 import java.util.ArrayDeque
 
 class Day14 : Day {
-    override val day = 14
+	override val day = 14
 
-    data class Robot(
-        val position: Cord2D<Int>,
-        val velocity: Cord2D<Int>,
-    ) {
-        fun move(bounds: Cord2D<Int>): Robot = this.copy(position = (this.position + this.velocity) % bounds)
+	data class Robot(
+		val position: Cord2D<Int>,
+		val velocity: Cord2D<Int>,
+	) {
+		fun move(bounds: Cord2D<Int>): Robot = this.copy(position = (this.position + this.velocity) % bounds)
 
-        fun moveXSteps(
-            bounds: Cord2D<Int>,
-            steps: Int,
-        ): Robot = this.copy(position = (this.position + this.velocity * steps) % bounds)
-    }
+		fun moveXSteps(
+			bounds: Cord2D<Int>,
+			steps: Int,
+		): Robot = this.copy(position = ((this.position + this.velocity * steps) % bounds))
+	}
 
-    fun List<Robot>.countInBounds(
-        start: Cord2D<Int>,
-        end: Cord2D<Int>,
-    ): Int =
-        this.count {
-            it.position.x > start.x &&
-                it.position.x < end.x &&
-                it.position.y > start.y &&
-                it.position.y < end.y
-        }
+	fun List<Robot>.countInBounds(
+		start: Cord2D<Int>,
+		end: Cord2D<Int>,
+	): Int =
+		this.count {
+			it.position.x > start.x &&
+				it.position.x < end.x &&
+				it.position.y > start.y &&
+				it.position.y < end.y
+		}
 
-    fun List<Robot>.printMap(bounds: Cord2D<Int>) {
-        (0 until bounds.y).forEach { y ->
-            (0 until bounds.x).forEach { x ->
-                val count = this.count { it.position.x == x && it.position.y == y }
-                if (count == 0) {
-                    print(" ")
-                } else {
-                    print(count)
-                }
-            }
-            println(".")
-        }
-    }
+	fun List<Robot>.printMap(bounds: Cord2D<Int>) {
+		(0 until bounds.y).forEach { y ->
+			(0 until bounds.x).forEach { x ->
+				val count = this.count { it.position.x == x && it.position.y == y }
+				if (count == 0) {
+					print(".")
+				} else {
+					print("#")
+				}
+			}
+			println(".")
+		}
+	}
 
-    val start by lazy {
-        loadInput()
-            .parseWithRegex("p=(\\d+),(\\d+) v=(-?\\d+),(-?\\d+)")
-            .map { (xStr, yStr, xVelStr, yVelStr) ->
-                Robot(
-                    Cord2D(xStr.toInt(), yStr.toInt()),
-                    Cord2D(xVelStr.toInt(), yVelStr.toInt()),
-                )
-            }
-    }
+	val start by lazy {
+		loadInput()
+			.parseWithRegex("p=(\\d+),(\\d+) v=(-?\\d+),(-?\\d+)")
+			.map { (xStr, yStr, xVelStr, yVelStr) ->
+				Robot(
+					Cord2D(xStr.toInt(), yStr.toInt()),
+					Cord2D(xVelStr.toInt(), yVelStr.toInt()),
+				)
+			}
+	}
 
-    override fun solvePart1() {
-        val bounds = Cord2D(101, 103)
-        start
-            .let {
-                (1..100).fold(it) { acc, i ->
-                    acc.map { it.move(bounds) }
-                }
-            }.let { robots ->
-                listOf(
-                    Cord2D(-1, -1) to Cord2D(bounds.x / 2, bounds.y / 2),
-                    Cord2D(bounds.x / 2, -1) to Cord2D(bounds.x, bounds.y / 2),
-                    Cord2D(-1, bounds.y / 2) to Cord2D(bounds.x / 2, bounds.y),
-                    Cord2D(bounds.x / 2, bounds.y / 2) to bounds,
-                ).map {
-                    robots.countInBounds(it.first, it.second).toLong()
-                }.reduce { acc, it -> acc * it }
-            }.solution(1)
-    }
+	override fun solvePart1() {
+		val bounds = Cord2D(101, 103)
+		start
+			.let {
+				(1..100).fold(it) { acc, i ->
+					acc.map { it.move(bounds) }
+				}
+			}.let { robots ->
+				listOf(
+					Cord2D(-1, -1) to Cord2D(bounds.x / 2, bounds.y / 2),
+					Cord2D(bounds.x / 2, -1) to Cord2D(bounds.x, bounds.y / 2),
+					Cord2D(-1, bounds.y / 2) to Cord2D(bounds.x / 2, bounds.y),
+					Cord2D(bounds.x / 2, bounds.y / 2) to bounds,
+				).map {
+					robots.countInBounds(it.first, it.second).toLong()
+				}.reduce { acc, it -> acc * it }
+			}.solution(1)
+	}
 
-    fun List<Robot>.areInOneShape(): Boolean {
-        val shapes = mutableListOf<Set<Cord2D<Int>>>()
-        val map =
-            this
-                .groupBy { it.position }
-                .toMutableMap()
-        while (map.isNotEmpty()) {
-            val queue = ArrayDeque<Cord2D<Int>>()
-            val seen = mutableSetOf<Cord2D<Int>>()
-            queue.add(map.keys.first())
-            while (queue.size > 0) {
-                val next = queue.removeFirst()
-                seen.add(next)
-                next
-                    .getNeighbors()
-                    .filter { it !in seen }
-                    .filter { it in map }
-                    .forEach { queue.add(it) }
-            }
-            shapes.add(seen)
-            seen.forEach { map.remove(it) }
-        }
+	fun List<Robot>.areInOneShape(): Boolean {
+		val shapes = mutableListOf<Set<Cord2D<Int>>>()
+		val map =
+			this
+				.groupBy { it.position }
+				.toMutableMap()
+		while (map.isNotEmpty()) {
+			val queue = ArrayDeque<Cord2D<Int>>()
+			val seen = mutableSetOf<Cord2D<Int>>()
+			queue.add(map.keys.first())
+			while (queue.size > 0) {
+				val next = queue.removeFirst()
+				seen.add(next)
+				next
+					.getNeighbors()
+					.filter { it !in seen }
+					.filter { it in map }
+					.filter { it !in queue }
+					.forEach { queue.add(it) }
+			}
+			shapes.add(seen)
+			seen.forEach { map.remove(it) }
+		}
 
-        return shapes.size < 400
-    }
+		return shapes.size < 200
+	}
 
-    fun findCycle(
-        start: Int,
-        velocity: Int,
-        bound: Int,
-    ): Long =
-        generateSequence(1L) { it + 1L }
-            .dropWhile { (it * velocity) % bound.toLong() != 0L }
-            .first()
+	fun findCycle(
+		start: Int,
+		velocity: Int,
+		bound: Int,
+	): Long =
+		generateSequence(1L) { it + 1L }
+			.dropWhile { (it * velocity) % bound.toLong() != 0L }
+			.first()
 
-    fun Robot.findCycle(bounds: Cord2D<Int>): Long {
-        val x = findCycle(this.position.x, this.velocity.x, bounds.x)
-        val y = findCycle(this.position.y, this.velocity.y, bounds.y)
+	fun Robot.findCycle(bounds: Cord2D<Int>): Long {
+		val x = findCycle(this.position.x, this.velocity.x, bounds.x)
+		val y = findCycle(this.position.y, this.velocity.y, bounds.y)
 
-        return lcm(x, y)
-    }
+		return lcm(x, y)
+	}
 
-    override fun solvePart2() {
-        val bounds = Cord2D(101, 103)
+	override fun solvePart2() {
+		val bounds = Cord2D(101, 103)
 
-        generateSequence(1) {
-            it + 1
-        }.dropWhile { steps ->
-            val map = start.map { it.moveXSteps(bounds, steps) }
+		val step = generateSequence(1) {
+			it + 1
+		}.dropWhile { steps ->
+			val map = start.map { it.moveXSteps(bounds, steps) }
 
-            !map.areInOneShape()
-        }.first()
-            .solution(2)
-    }
+			!map.areInOneShape()
+		}.first()
+
+		start.map { it.moveXSteps(bounds, step) }
+			.printMap(bounds)
+
+		step
+			.solution(2)
+	}
 }
 
 private operator fun Cord2D<Int>.times(steps: Int): Cord2D<Int> = Cord2D(this.x * steps, this.y * steps)
 
 private operator fun Cord2D<Int>.rem(bounds: Cord2D<Int>): Cord2D<Int> {
-    if (this.x >= 0 &&
-        this.x < bounds.x &&
-        this.y >= 0 &&
-        this.y < bounds.y
-    ) {
-        return this
-    }
 
-    return Cord2D((this.x + bounds.x) % bounds.x, (this.y + bounds.y) % bounds.y)
+
+	var y = (this.y) % bounds.y
+	var x = (this.x) % bounds.x
+	if(x < 0) x = x + bounds.x
+	if(y < 0) y = y + bounds.y
+	return Cord2D(x,y)
 }
 
 fun main() = solve<Day14>()
---
+++ main/days/Day15.kt
@@ -0,0 +1,194 @@
+package me.reckter.aoc.days
+
+import me.reckter.aoc.Day
+import me.reckter.aoc.cords.d2.Cord2D
+import me.reckter.aoc.cords.d2.plus
+import me.reckter.aoc.days.Day15.Direction.Left
+import me.reckter.aoc.days.Day15.Direction.Right
+import me.reckter.aoc.days.Day15.Tile.Box
+import me.reckter.aoc.days.Day15.Tile.BoxLeft
+import me.reckter.aoc.days.Day15.Tile.BoxRight
+import me.reckter.aoc.days.Day15.Tile.Free
+import me.reckter.aoc.days.Day15.Tile.Robot
+import me.reckter.aoc.days.Day15.Tile.Wall
+import me.reckter.aoc.parseMap
+import me.reckter.aoc.solution
+import me.reckter.aoc.solve
+import me.reckter.aoc.splitAt
+
+class Day15 : Day {
+	override val day = 15
+
+
+	enum class Tile {
+		Free,
+		Box,
+		BoxLeft,
+		BoxRight,
+		Wall,
+		Robot
+	}
+
+	enum class Direction(val vector: Cord2D<Int>) {
+		Up(Cord2D(0, -1)),
+		Right(Cord2D(1, 0)),
+		Down(Cord2D(0, 1)),
+		Left(Cord2D(-1, 0))
+	}
+
+	val startMap by lazy {
+		loadInput( trim = false)
+			.splitAt { it.isEmpty() }
+			.first()
+			.toList()
+			.parseMap {
+				when (it) {
+					'.' -> Tile.Free
+					'O' -> Tile.Box
+					'#' -> Tile.Wall
+					'@' -> Tile.Robot
+					else -> error("no definition for '$it'")
+				}
+			}
+	}
+
+	fun Map<Cord2D<Int>, Tile>.print(robot: Cord2D<Int>) {
+		val minX = this.keys.minOf { it.x }
+		val maxX = this.keys.maxOf { it.x }
+		val minY = this.keys.minOf { it.y }
+		val maxY = this.keys.maxOf { it.y }
+
+		(minY..maxY).forEach { y ->
+			(minX..maxX).forEach { x ->
+
+				val tile = if (robot.x == x && robot.y == y) Tile.Robot else this[Cord2D(x, y)] ?: Tile.Free
+				print(
+					when (tile) {
+						Free -> "."
+						Box -> "O"
+						BoxLeft -> "["
+						BoxRight -> "]"
+						Wall -> "#"
+						Robot -> "@"
+					}
+				)
+			}
+			println()
+		}
+	}
+
+	val instructions by lazy {
+		loadInput( trim = false)
+			.splitAt { it.isEmpty() }
+			.last()
+			.joinToString("")
+			.map {
+				when (it) {
+					'^' -> Direction.Up
+					'>' -> Direction.Right
+					'v' -> Direction.Down
+					'<' -> Direction.Left
+					else -> error("Invalid direction: '$it'")
+				}
+			}
+	}
+
+	fun Map<Cord2D<Int>, Tile>.tryMove(
+		robot: Cord2D<Int>,
+		direction: Direction
+	): Pair<Cord2D<Int>, Map<Cord2D<Int>, Tile>> {
+		val next = robot + direction.vector
+
+		val queue = ArrayDeque<Cord2D<Int>>()
+		val seen = mutableSetOf<Cord2D<Int>>()
+		queue.add(next)
+
+		while (queue.size > 0) {
+			val n = queue.removeFirst()
+
+			if (n in seen) continue
+
+			// we hit a wall, abort
+			if (this[n] == Wall) return robot to this
+
+			if (this[n] !in listOf(Tile.Box, BoxRight, BoxLeft)) {
+				// we hit empty space, nothing to do
+				continue
+			}
+
+			seen.add(n)
+
+			if (this[n] == BoxLeft) {
+				queue.add(n + direction.vector)
+				queue.add(n + Cord2D(1, 0))
+			} else if (this[n] == BoxRight) {
+				queue.add(n + direction.vector)
+				queue.add(n + Cord2D(-1, 0))
+			} else {
+				queue.add(n + direction.vector)
+			}
+		}
+
+		val newMap = this - seen +
+			seen.map { (it + direction.vector) to (this[it] ?: error(" No box? at $it")) }
+
+		return next to newMap
+	}
+
+	override fun solvePart1() {
+		val robot = startMap.filter { it.value == Robot }
+			.entries
+			.single()
+			.key
+
+		val mapWithoutRobot = startMap - robot
+
+		instructions
+			.fold(robot to mapWithoutRobot) { current, instr ->
+				current.second.tryMove(current.first, instr)
+			}
+			.second
+			.filter { it.value == Box }
+			.keys
+			.sumOf { it.x + it.y * 100 }
+			.solution(1)
+	}
+
+	override fun solvePart2() {
+		val dublicateDirection = Cord2D(1,0)
+		val map = startMap
+			.entries.flatMap {
+				val pos = Cord2D(it.key.x + it.key.x, it.key.y)
+				when(it.value){
+					Free -> listOf(pos to it.value, (pos + dublicateDirection) to it.value)
+					Box ->  listOf(pos to BoxLeft, (pos + dublicateDirection) to BoxRight)
+					BoxLeft -> TODO()
+					BoxRight -> TODO()
+					Wall -> listOf(pos to it.value, pos + dublicateDirection to it.value)
+					Robot -> listOf(pos to it.value, pos + dublicateDirection to Free)
+				}
+			}
+			.toMap()
+
+		val robot = map.filter { it.value == Robot }
+			.entries
+			.single()
+			.key
+
+		val mapWithoutRobot = map - robot
+
+		instructions
+			.fold(robot to mapWithoutRobot) { current, instr ->
+				val res = current.second.tryMove(current.first, instr)
+
+				res
+			}
+			.second
+			.filter { it.value == BoxLeft }
+			.keys
+			.sumOf { it.x + it.y * 100 }
+			.solution(2)
+	}
+}
+
+fun main() = solve<Day15>()
